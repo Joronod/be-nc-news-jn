cc08c10c649337604c55f6802f9c66d1e7978b31
John <jrnoden@googlemail.com>
2024-05-22 17:42:29 +0100

    This adds error handling for GET articles by ID and refactors original endpoint

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -2,8 +2,6 @@ const { selectArticleById } = require("../models/articles.models")
 
 exports.getArticleById = (req, res, next) =>{
     const { article_id } = req.params
-    
-    console.log(article_id)
     return selectArticleById(article_id)
     .then((article)=>{
         res.status(200).send(article[0])
