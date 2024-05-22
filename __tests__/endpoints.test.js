73d3a361d7f6adb6fd7db93785f5518a15bd6b98
John <jrnoden@googlemail.com>
2024-05-22 17:55:58 +0100

    "this will update the endpoints.json file with the GET atricle by ID endpoint and pass the associated testing"

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -45,3 +45,32 @@ describe("GET: /api", ()=>{
         })
     })
 })
+
+describe("GET: /api/articles/:article_id", ()=>{
+    test.only("200: responds with an article when given a valid article ID", ()=>{
+        return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body })=>{
+            expect(body).toMatchObject({
+                article_id: 1,
+                title: "Living in the shadow of a great man",
+                topic: "mitch",
+                author: "butter_bridge",
+                body: "I find this existence challenging",
+                created_at: '2020-07-09T20:11:00.000Z',
+                votes: 100,
+                article_img_url:
+                  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",         
+            })
+        })
+    })
+    test("404: ERROR - responds with an error when the id is valid, but does not exist", ()=>{
+        return request(app)
+        .get("/api/articles/9876")
+        .expect(404)
+        .then(({ body })=>{
+            expect(body.msg).toBe("Not Found")
+        })
+    })
+})
