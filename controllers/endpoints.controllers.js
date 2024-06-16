b79e78f41754081f5da0b4c3bfa6585c9c38c6a5
John <jrnoden@googlemail.com>
2024-06-16 21:02:18 +0100

    attempts to fix /api display issue

--- controllers/endpoints.controllers.js
+++ controllers/endpoints.controllers.js
@@ -3,7 +3,7 @@ const fs = require("fs/promises")
 exports.getAllEndpoints = (req, res, next)=>{
     return fs.readdir("../be-nc-news")
     .then(()=>{
-       return fs.readFile("endpoints.json")
+       return fs.readFile("./endpoints.json")
     })
     .then((results)=>{
         return res.status(200).send({status: 200, msg: JSON.parse(results)})
